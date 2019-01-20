f76fad96b0a57e9593d1278d1128300c038723b4
Sleaf <sleaf1996@qq.com>
2019-01-21 00:33:56 +0800

    fix: 更新LazyPageWrapper用法

--- src/utils/hocs/LazyPageWrapper.js
+++ src/utils/hocs/LazyPageWrapper.js
@@ -1,8 +1,15 @@
 import React from "react";
 import Loading from "@/components/Loading";
 
-export default Component => props => (
-  <React.Suspense fallback={<Loading />}>
-    <Component {...props} />
-  </React.Suspense>
-);
+/**
+ * @example
+ * const LazyComponent=LazyPageWrapper(import('@/components/Example.js'))
+ * */
+export default impComponent => props => {
+  const Component = React.lazy(_ => impComponent);
+  return (
+    <React.Suspense fallback={<Loading />}>
+      <Component {...props} />
+    </React.Suspense>
+  )
+};
