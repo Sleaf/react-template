10b608b11d695fb319533a49a9d8ad86cdd7c86f
Sleaf <shu.huang@mail.hypers.com>
2020-06-18 14:42:37 +0800

    chore: 优化dev控制台输出

--- webpack.dev.ts
+++ webpack.dev.ts
@@ -2,6 +2,7 @@ import fs from 'fs';
 import os from 'os';
 import { resolve } from 'path';
 import merge from 'webpack-merge';
+import FriendlyErrorsWebpackPlugin from 'friendly-errors-webpack-plugin';
 import common, { isWindows, PUBLIC_PATH } from './webpack.common';
 import mockServer from './mockServer/index';
 
@@ -19,27 +20,39 @@ const availableIpv4 = Object.values(ips)
   .reduce((acc, item) => acc.concat(item), [])
   .map(item => item.address);
 
+const devServer = {
+  publicPath: PUBLIC_PATH,
+  disableHostCheck: true,
+  historyApiFallback: true,
+  compress: true,
+  quiet: true,
+  overlay: true,
+  host: ENABLE_SSL ? 'localhost' : availableIpv4[0] || (isWindows ? '127.0.0.1' : '0.0.0.0'),
+  port: exportPort,
+  https: ENABLE_SSL && {
+    key: fs.readFileSync(resolve(__dirname, 'ssl/ssl.localhost.key')),
+    cert: fs.readFileSync(resolve(__dirname, 'ssl/ssl.localhost.crt')),
+  },
+  before: ENABLE_MOCK_SERVER ? mockServer : undefined,
+  proxy: {
+    '/api': {
+      target: 'http://localhost',
+      secure: false,
+      changeOrigin: true,
+    },
+  },
+};
+
 export default merge(common, {
   mode: 'development',
   devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    publicPath: PUBLIC_PATH,
-    disableHostCheck: true,
-    historyApiFallback: true,
-    compress: true,
-    host: ENABLE_SSL ? 'localhost' : availableIpv4[0] || (isWindows ? '127.0.0.1' : '0.0.0.0'),
-    port: exportPort,
-    https: ENABLE_SSL && {
-      key: fs.readFileSync(resolve(__dirname, 'ssl/ssl.localhost.key')),
-      cert: fs.readFileSync(resolve(__dirname, 'ssl/ssl.localhost.crt')),
-    },
-    before: ENABLE_MOCK_SERVER ? mockServer : undefined,
-    proxy: {
-      '/api': {
-        target: 'http://localhost',
-        secure: false,
-        changeOrigin: true,
+  devServer,
+  plugins: [
+    new FriendlyErrorsWebpackPlugin({
+      compilationSuccessInfo: {
+        messages: [`Web is running here: http${ENABLE_SSL ? 's' : ''}://${devServer.host}:${devServer.port}`],
       },
-    },
-  },
+      clearConsole: true,
+    }),
+  ],
 });
