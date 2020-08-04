584e73fddb2d815a362fb1468fd4059109a8640a
Sleaf <shu.huang@mail.hypers.com>
2020-08-04 15:23:24 +0800

    fix: 移除polyfill残余代码

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,6 @@
 module.exports = {
   presets: [
-    [
-      '@babel/preset-env',
-      {
-        useBuiltIns: 'usage',
-        corejs: { version: 3, proposals: true }
-      },
-    ],
+    '@babel/preset-env',
     '@babel/preset-typescript',
     '@babel/preset-react',
   ],
